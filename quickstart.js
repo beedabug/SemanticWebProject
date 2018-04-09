72c89caa4f2b17f9b157e4ae0e3969efd13c1e1a
Snehal Pathak <snehal@android-18361d7c7f8fb28f.utdallas.edu>
2018-04-09 18:22:37 -0500

    implemented fuseki querying and visualization

--- quickstart.js
+++ quickstart.js
@@ -4,6 +4,7 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
       var data = new google.visualization.DataTable();
       data.addColumn('number', 'Year');
+<<<<<<< HEAD
       data.addColumn('number', 'Percentage Preterm Births');
       //data.addColumn('number', 'Percentage Prenatal Care');
       /*data.addRows([
@@ -19,6 +20,20 @@ function drawChart() {
         [3,  25.4],
         [4,  11.7],
         [5,  11.9],
+=======
+      data.addColumn('number', '1st Trimester');
+      data.addColumn('number', '2nd Trimester');
+      data.addColumn('number', '3rd Trimester');
+      data.addColumn('number', 'None');
+      data.addColumn('number', 'Preterm births');
+
+      data.addRows([
+        [1999, 37.8, 80.8, 30.9, 69.5, 20],
+        [2000, 30.9, 69.5, 37.8, 80.8, 30],
+        [2001, 25.4, 57, 11.7, 18.8, 40],
+        [2002, 11.7, 18.8, 5.4, 57, 60],
+        [2003, 11.9, 17.6, 29, 34.2, 20],
+>>>>>>> f7256ba0b806233ecfff0e8b462bb75edb734c7f
       ]);
 
        var options = {
@@ -30,8 +45,8 @@ function drawChart() {
 
       /*var options = {
         chart: {
-          title: 'Box Office Earnings in First Two Weeks of Opening',
-          subtitle: 'in millions of dollars (USD)'
+          title: 'Prenatal Care vs. Preterm Births in Chicago',
+          subtitle: 'in Percentage per Year'
         },
         width: 900,
         height: 500
@@ -41,4 +56,3 @@ function drawChart() {
 
       chart.draw(data, options);
     }
-
