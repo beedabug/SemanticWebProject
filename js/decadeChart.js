3b25d8b302929e02442e372971db0436d1893ec0
beedabug <beedabug@users.noreply.github.com>
2018-04-20 22:53:07 -0500

    Update decadeChart.js

--- js/decadeChart.js
+++ js/decadeChart.js
@@ -22,8 +22,8 @@ function drawColumnChart() {
         hAxis: {
           format: '',
           viewWindow: {
-            min: 1999,
-            max: 2009
+            min: 1998,
+            max: 2010
           },
           title: 'Year'
         },
